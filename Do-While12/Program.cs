b5a543780c8063b08d1d8d0484f0a73a71f5fe7d
dgTr3 <Edugta31@gmail.com>
2026-03-05 20:27:49 -0600

    completo

--- Do-While12/Program.cs
+++ Do-While12/Program.cs
@@ -1,1 +1,11 @@
 ﻿Console.WriteLine("Pedir números hasta qeu ingrese el 0");
+int suma = 0;
+int numero;
+do
+{
+
+   Console.WriteLine("Ingrese un número: ");
+    numero = int.Parse(Console.ReadLine());
+    suma += numero;
+} while (numero != 0);
+Console.WriteLine("La suma total: " + suma);
